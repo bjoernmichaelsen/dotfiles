458f6575163b14e20bd156ddda7d90d3f8fddd57
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2023-07-23 21:18:11 +0200

    more eyecandy for nvim

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -11,14 +11,21 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	{
-  		"folke/tokyonight.nvim",
+ 	{
+   		"folke/tokyonight.nvim",
   		lazy = false,
   		priority = 1000,
   		opts = {},
-	}
+ 	}
 })
-vim.cmd([[colorscheme tokyonight-night]])
+require("tokyonight").setup({
+	style = "night",
+ 	on_colors = function(colors)
+		colors.bg = "#000000"
+		colors.bg_dark= "#000000"
+ 	end
+})
 vim.cmd([[set bg=dark]])
+vim.cmd([[colorscheme tokyonight]])
 vim.cmd([[set ts=4 sw=4 et]])
 vim.cmd([[filetype plugin on]])
